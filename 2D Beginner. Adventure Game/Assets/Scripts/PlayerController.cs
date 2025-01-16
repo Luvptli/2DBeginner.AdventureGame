78518050822947eaec6ce23aeb525598aa31c8c0
Luvptli <lucxx041@gmail.com>
2025-01-16 12:07:35 +0000

    Movimiento y creación mundo
    Movimiento simplificado y terminado, añadido flechas AWSD, joystick. Comienzo de edición del mundo

--- 2D Beginner. Adventure Game/Assets/Scripts/PlayerController.cs
+++ 2D Beginner. Adventure Game/Assets/Scripts/PlayerController.cs
@@ -6,20 +6,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    /*public InputAction LeftAction;*/
+    public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*LeftAction.Enable();*/
+        MoveAction.Enable();
+        /*QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;*/
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
+
         //variable de nombre horizontal (para controlar el movimiento horizontal en el eje x)
-        float horizontal = 0.0f;
+        /*float horizontal = 0.0f;
+        
+        float vertical = 0.0f;
 
         //si presonas la flecha a la derecha el horizontal pasa a ser -1
-        if (Keyboard.current.leftArrowKey.isPressed)
+        /*if (Keyboard.current.leftArrowKey.isPressed)*/
+        /*if (LeftAction.IsPressed())
         {
             horizontal = -1.0f;
         }
@@ -29,24 +41,37 @@ public class PlayerController : MonoBehaviour
             horizontal = 1.0f;
         }
 
+        else if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+
         //en la consola te aparece la información de la variable horizontal
         Debug.Log(horizontal);
+        Debug.Log(vertical);
 
         //crea un nuevo vector en 2D con el nombre position que es igual a la posición en la que se movió
         Vector2 position = transform.position;
 
         //lleva el position uno más a la derecha en el 
         position.x = position.x + 0.1f * horizontal;
+        position.y = position.y + 0.1f * vertical;
 
         //mover el personaje en el eje x hacia alante
         /*position.x = position.x + 0.1f;/*
         transform.position = position;
 
         //mover al personaje en el eje y hacia arriba
-        /*position.y = position.y + 0.001f;
-        transform.position = position;*/
+        /*position.y = position.y + 0.001f;*/
+        transform.position = position;
 
         //si añades los dos a la vez el personaje se mueve en diagonal hacia la derecha y hacia arriba
+
 
     }
 }
